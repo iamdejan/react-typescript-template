8eebb393e7b13186934775cfc47d9d8ea149c639
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2024-09-14 22:32:40 +0700

    initial commit

---
+++ src/routes/index.lazy.tsx
@@ -0,0 +1,14 @@
+import { JSX } from "react";
+import { createLazyFileRoute } from "@tanstack/react-router";
+
+export const Route = createLazyFileRoute("/")({
+  component: Index,
+});
+
+function Index(): JSX.Element {
+  return (
+    <div className="p-2">
+      <h3>Welcome Home!</h3>
+    </div>
+  );
+}
